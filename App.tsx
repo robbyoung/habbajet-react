8cf4e26dc62658fd22677f51c031c9281190bbd8
Robert Young <robertyoung10101@gmail.com>
2020-03-01 08:46:38 +1300

    Added provider for state

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import {View, Text} from 'react-native';
+import {Provider} from 'react-redux';
+import store from './app/store';
 
 const App = () => {
-  return (
-      <View><Text>Habbajet</Text></View>
-  );
+    return (
+        <Provider store={store}>
+            <View>
+                <Text>Habbajet</Text>
+            </View>
+        </Provider>
+    );
 };
 
 export default App;
