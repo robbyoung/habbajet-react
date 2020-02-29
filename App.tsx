be6d2d56aa95c748b0a4eb4e6f66fd58b0a3ebff
Robert Young <robertyoung10101@gmail.com>
2020-02-29 21:33:55 +1300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+      <View><Text>Habbajet</Text></View>
+  );
+};
+
+export default App;
