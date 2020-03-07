65b39bccb84ca52461e7a7bc63824ebd82adc09e
Robert Young <robertyoung10101@gmail.com>
2020-03-08 10:59:18 +1300

    Added test for icon button component

--- App.tsx
+++ App.tsx
@@ -2,20 +2,11 @@ import React from 'react';
 import {View} from 'react-native';
 import {Provider} from 'react-redux';
 import store from './app/store';
-import IconButton from './app/components/iconButton';
-import {faCoffee} from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
     return (
         <Provider store={store}>
-            <View>
-                <IconButton
-                    color="#fc8803"
-                    size={70}
-                    icon={faCoffee}
-                    onPress={() => console.error('hey')}
-                />
-            </View>
+            <View />
         </Provider>
     );
 };
