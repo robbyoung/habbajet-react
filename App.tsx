efdc2bef36416dcf6cd3dfc0b08989b48e22e95a
Robert Young <robertyoung10101@gmail.com>
2020-03-08 10:48:52 +1300

    Added props to new icon button component

--- App.tsx
+++ App.tsx
@@ -3,12 +3,18 @@ import {View} from 'react-native';
 import {Provider} from 'react-redux';
 import store from './app/store';
 import IconButton from './app/components/iconButton';
+import {faCoffee} from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
     return (
         <Provider store={store}>
             <View>
-                <IconButton/>
+                <IconButton
+                    color="#fc8803"
+                    size={70}
+                    icon={faCoffee}
+                    onPress={() => console.error('hey')}
+                />
             </View>
         </Provider>
     );
--- app/components/iconButton.tsx
+++ app/components/iconButton.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import {View, StyleSheet, TouchableOpacity} from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
-
-const styles = StyleSheet.create({
-
-});
+import {TouchableOpacity} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {IconDefinition} from '@fortawesome/free-solid-svg-icons';
 
 interface IconButtonProps {
+    icon: IconDefinition;
+    size: number;
+    color: string;
+    onPress: () => void;
 }
 const IconButton = (props: IconButtonProps) => {
     return (
-        <TouchableOpacity>
-            <View>
-                <FontAwesomeIcon icon={faCheck}/>
-            </View>
+        <TouchableOpacity onPress={() => props.onPress()}>
+            <FontAwesomeIcon
+                icon={props.icon}
+                size={props.size}
+                color={props.color}
+            />
         </TouchableOpacity>
     );
 };
