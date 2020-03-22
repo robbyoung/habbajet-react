caa08ccaf21d1bb612ac3ce72605eb006c82a2cf
Robert Young <robertyoung10101@gmail.com>
2020-03-22 18:25:27 +1300

    Added an add habit result action

---
+++ app/actions/addHabitResult.tsx
@@ -0,0 +1,44 @@
+import moment from 'moment';
+import {Action} from 'redux';
+import {ActionType} from './actionTypes';
+import {Habbajet} from '../state';
+
+export interface AddHabitResultAction extends Action {
+    type: ActionType.ADD_HABIT_RESULT;
+    habbajetName: string;
+    success: boolean;
+}
+
+export function addHabitResultAction(
+    habbajetName: string,
+    success: boolean,
+): AddHabitResultAction {
+    return {
+        type: ActionType.ADD_HABIT_RESULT,
+        habbajetName,
+        success,
+    };
+}
+
+export function addHabitResult(
+    state: Habbajet[],
+    action: AddHabitResultAction,
+): Habbajet[] {
+    const selected = state.findIndex(
+        habbajet => habbajet.name === action.habbajetName,
+    );
+    if (selected === undefined) {
+        return state;
+    }
+
+    const newState = [...state];
+    const edited: Habbajet = {...newState[selected]};
+    const date = moment(edited.date);
+
+    date.add(1, 'day');
+    edited.date = date.toISOString();
+    if (action.success) {
+        edited.successes++;
+    }
+    return newState;
+}
--- app/actions/index.tsx
+++ app/actions/index.tsx
@@ -1,2 +1,3 @@
 export * from './actionTypes';
 export * from './addHabbajet';
+export * from './addHabitResult';
