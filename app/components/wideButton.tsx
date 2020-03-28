21e3754073e80a8a36aaba19a5a44cec8b3b9b07
Robert Young <robertyoung10101@gmail.com>
2020-03-29 08:43:44 +1300

    Added and tested a wide button component

---
+++ app/components/wideButton.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        borderRadius: 10,
+    },
+    text: {
+        fontSize: 30,
+        alignSelf: 'center',
+        fontFamily: 'Abel',
+        color: '#fff',
+        width: '100%',
+        textAlign: 'center',
+        padding: 10,
+    },
+});
+
+interface WideButton {
+    text: string;
+    color: string;
+    onPress: () => void;
+}
+const WideButton = (props: WideButton) => {
+    return (
+        <TouchableOpacity
+            onPress={() => props.onPress()}
+            style={[styles.container, {backgroundColor: props.color}]}>
+            <Text style={[styles.text]}>{props.text}</Text>
+        </TouchableOpacity>
+    );
+};
+
+export default WideButton;
--- app/containers/homeScreen.tsx
+++ app/containers/homeScreen.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import Label from '../components/label';
 import {getBudgetFormatted} from '../selectors';
+import {View} from 'react-native';
 
 const HomeScreen = () => {
     const budget = useSelector(getBudgetFormatted);
 
     return (
-        <Label
-            title="My Budget"
-            content={budget}
-            color="#959595"
-            contentSize={50}
-        />
+        <View>
+            <Label
+                title="My Budget"
+                content={budget}
+                color="#959595"
+                contentSize={50}
+            />
+        </View>
     );
 };
 
