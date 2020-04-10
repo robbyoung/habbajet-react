7c62de44aa02343437c578253e1074db425ed365
Robert Young <robertyoung10101@gmail.com>
2020-04-10 17:51:52 +1200

    Added some new colors

--- app/colors.tsx
+++ app/colors.tsx
@@ -6,16 +6,22 @@ export const errorRed = '#b02f1e';
 
 export const habbajetColors = [
     '#8066C2', // Purple
-    '#2AAD5E', // Green
     '#E04747', // Red
-    '#7F90EC', // Blue
+    '#2AAD5E', // Green
     '#B1B952', // Gold
+    '#7F90EC', // Blue
+    '#37353C', // Black
+    '#E64CE9', // Pink
+    '#EB712D', // Orange
 ];
 
 export const complementColors: {[id: string]: string} = {
     '#8066C2': '#C2A066',
+    '#E04747': '#6E6E6E',
     '#2AAD5E': '#AD372A',
-    '#E04747': '#47E047',
-    '#7F90EC': '#ECB27F',
     '#B1B952': '#B952B0',
+    '#7F90EC': '#ECB27F',
+    '#37353C': '#FF5252',
+    '#E64CE9': '#E9E64C',
+    '#EB712D': '#6E6E6E',
 };
