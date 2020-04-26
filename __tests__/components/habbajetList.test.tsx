399f7d032e8ac624fdb49033702920f879d4c17c
Robert Young <robertyoung10101@gmail.com>
2020-04-27 09:05:32 +1200

    Highlight habbajet buttons with pending actions

--- __tests__/components/habbajetList.test.tsx
+++ __tests__/components/habbajetList.test.tsx
@@ -2,9 +2,23 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import {createTestState} from '../../app/state/testState';
 import HabbajetList from '../../app/components/habbajetList';
+import moment from 'moment';
 
 describe('HabbajetList Component', () => {
     it('will create buttons out of an array of habbajets', () => {
+        const habbajets = createTestState(30, 0, 0).habbajets;
+        habbajets.forEach(habbajet => {
+            habbajet.date = moment()
+                .add(1, 'day')
+                .toISOString();
+        });
+        const component = renderer.create(
+            <HabbajetList habbajets={habbajets} onSelect={() => undefined} />,
+        );
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+
+    it('will highlight habits with pending actions', () => {
         const habbajets = createTestState(30, 0, 0).habbajets;
         const component = renderer.create(
             <HabbajetList habbajets={habbajets} onSelect={() => undefined} />,
